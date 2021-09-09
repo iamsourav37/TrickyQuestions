a447a76404f5645bd2389ad0e38807fc93641550
Sourav Ganguly <iamsourav37@gmail.com>
2021-09-09 18:47:13 +0530

    print 1 to 100 without using numeric value

---
+++ src/Print1To100.java
@@ -0,0 +1,31 @@
+ 
+/**
+ * print 1 to 100 without using any numbers
+ */
+
+
+public class Print1To100 {
+    public static void main(String[] args) {
+
+        int one = 'A'/'A';
+
+        String str = "..........";
+
+
+        for (int i=one; i<= (str.length()* str.length()); i++) {
+            System.out.println(i);
+        }
+
+        System.out.println("Second way");
+
+        int hundred = (int) 'd';
+
+
+        // from zero
+        for(int i='a'%'a'; i<=hundred; i++){
+            System.out.println(i);
+        }
+
+    }
+    
+}

707c270976c02064b7dfb6cdfb0890eb88123ed6
Sourav Ganguly <iamsourav37@gmail.com>
2021-09-09 21:34:15 +0530

    print without semicolon(;)

---
+++ src/PrintHelloWorld.java
@@ -0,0 +1,22 @@
+/**
+ * print hello world without using semicolon (;)
+ */
+
+public class PrintHelloWorld {
+    public static void main(String[] args) {
+
+        if(System.out.printf("Hello World") == null){}
+
+        if(System.out.append("Hello World") == null){}
+
+        for(int i=0; i<1; System.out.println("Hello World !!!")){
+            i++;
+        }
+
+    }
+
+   
+    
+    
+}
+
